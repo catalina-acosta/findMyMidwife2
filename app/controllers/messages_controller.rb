37513cc18a03a26fce875b6a3f6388032a91d22b
catalina-acosta <catalina.acosta.rivera@gmail.com>
2025-06-03 14:52:31 +0200

    added new create method to MessagesController to see if message display in production runs without an error

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,16 +9,40 @@ class MessagesController < ApplicationController
     @message = Message.new(message_params)
     @message.booking = @booking
     @message.user = current_user
+
     if @message.save
       BookingChannel.broadcast_to(
         @booking,
-        render_to_string(partial: "message", locals: {message: @message})
+        render_to_string(partial: "message", locals: { message: @message })
       )
-      head :ok
+      respond_to do |format|
+        format.turbo_stream { head :ok }
+        format.html { redirect_to booking_path(@booking) }
+        format.json { render json: @message, status: :created }
+      end
     else
-      render "bookings/show", status: :unprocessable_entity
+      respond_to do |format|
+        format.html { render "bookings/show", status: :unprocessable_entity }
+        format.json { render json: @message.errors, status: :unprocessable_entity }
+      end
     end
   end
+  
+  # def create
+  #   @booking = Booking.find(params[:booking_id])
+  #   @message = Message.new(message_params)
+  #   @message.booking = @booking
+  #   @message.user = current_user
+  #   if @message.save
+  #     BookingChannel.broadcast_to(
+  #       @booking,
+  #       render_to_string(partial: "message", locals: {message: @message})
+  #     )
+  #     head :ok
+  #   else
+  #     render "bookings/show", status: :unprocessable_entity
+  #   end
+  # end
 
   private
 
