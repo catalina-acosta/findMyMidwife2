1378a83073012c1576e183ffe293d6b2b3eefc1a
catalina-acosta <catalina.acosta.rivera@gmail.com>
2025-06-03 10:19:52 +0200

    added ActionCable to routes.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   config.action_cable.url = "wss://findmymidwife2.onrender.com/cable"
- config.action_cable.allowed_request_origins = [ "https://findmymidwife2.onrender.com" ]
+  config.action_cable.allowed_request_origins = [ "https://findmymidwife2.onrender.com" ]
   # , /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
